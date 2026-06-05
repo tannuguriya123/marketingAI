60cd4080f5fd09ae934518d61303072d60988fd0
tannuguriya123 <tannu.gudiya1988@gmail.com>
2026-06-05 14:05:09 +0530

    Update vite.config.js

--- frontend/marketing_frontend/vite.config.js
+++ frontend/marketing_frontend/vite.config.js
@@ -2,6 +2,19 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
+export default defineconfig( {
+  plugins: [react(), tailwindcss()],
+  server: {
+    proxy: {
+      '/api' : {
+         target : 'https://marketingai-kzfn.onrender.com',
+         changeOrigin : true,
+         secure : false,
+         rewrite : (path) => path.replace(/^\/api/, '')
+      }
+    }
+  }
+} )
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
