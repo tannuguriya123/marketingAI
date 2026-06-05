e101d78bd001512e365493854f75591118e7919c
tannuguriya123 <tannu.gudiya1988@gmail.com>
2026-06-05 15:34:42 +0530

    ...

--- frontend/marketing_frontend/vite.config.js
+++ frontend/marketing_frontend/vite.config.js
@@ -13,7 +13,7 @@ export default defineConfig({
         target: 'https://marketingai-kzfn.onrender.com',
         changeOrigin: true,
         secure: false, // Set to true if your backend uses a valid SSL certificate
-        rewrite: (path) => path.replace(/^\/api/, '')
+    
       }
     }
   }
